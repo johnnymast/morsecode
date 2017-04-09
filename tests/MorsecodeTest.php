1e1e19914aa62519cb0c82af7fed66024d529396
Johnny Mast <mastjohnny@gmail.com>
2017-04-09 12:23:33 +0200

    Updated the code for PSR-2

--- tests/MorsecodeTest.php
+++ tests/MorsecodeTest.php
@@ -29,4 +29,30 @@ class MorsecodeTest extends \PHPUnit_Framework_TestCase
 
         $this->assertEquals($expected, $actual);
     }
+
+    /**
+     * Test that encode will work if the message is passed via
+     * the constructor of the class.
+     */
+    function test_it_encodes_if_the_constructor_is_used()
+    {
+        $morse = new Morsecode('sos');
+        $actual = $morse->encode();
+        $expected = '... --- ...';
+
+        $this->assertEquals($expected, $actual);
+    }
+
+    /**
+     * Test that decode will work if the message is passed via
+     * the constructor of the class.
+     */
+    function test_it_decodes_if_the_constructor_is_used()
+    {
+        $morse = new Morsecode('... --- ...');
+        $actual = $morse->decode();
+        $expected = 'sos';
+
+        $this->assertEquals($expected, $actual);
+    }
 }
